00c66e9cebb8fdced30178af5f6a2ea69ad3c128
Adam <adam.cottis@outlook.com>
2020-10-04 01:22:38 +0100

    Copy and Paste working.

--- popup.js
+++ popup.js
@@ -1,31 +1,58 @@
-const get_tabs = document.querySelector('#get_tabs');
-const test_label = document.querySelector('#output');
+const copy_tabs = document.querySelector('#copy_tabs');
+const paste_tabs = document.querySelector('#paste_tabs');
+const output = document.querySelector('#output');
+
+// Even Listening for the copy tabs button
+copy_tabs.addEventListener("click", async (e) => {
+    // Calls getUrls
+    copyUrls();
+    // browser.tabs.executeScript({file: "/content.js"});
+})
 
 // Even Listening for the Get Tabs button
-get_tabs.addEventListener("click", async (e) => {
-
+paste_tabs.addEventListener("click", async (e) => {
     // Calls getUrls
-    getUrls();
+    pasteUrls();
     // browser.tabs.executeScript({file: "/content.js"});
 })
 
 // Function that gets a list of tabs and loops through them.
-const getUrls = async () => {
+const copyUrls = async () => {
 
     // Stores the text to write to send to clipboard
     let clipboard_buffer = "";
 
     // Gets the list of tabs from the FirefoxAPI
-    let tabs = await browser.tabs.query({ currentWindow: true });
+    const tabs = await browser.tabs.query({ currentWindow: true });
 
     // Loops through the tabs and adds the url to the buffer
     tabs.forEach((tab) => {
-        test_label.innerHTML = tab.url;
-        clipboard_buffer = clipboard_buffer + tab.url + "\n"
-
+        output.innerHTML = tab.url;
+        clipboard_buffer = clipboard_buffer + tab.url + "\n";
     })
+    clipboard_buffer = clipboard_buffer.slice(0, -1);
 
     // Pastes tabs to clipboard
-    navigator.clipboard.writeText(clipboard_buffer)
+    navigator.clipboard.writeText(clipboard_buffer);
     //navigator.clipboard.write(tabs);
+}
+
+
+// Function that gets a list of tabs and loops through them.
+const pasteUrls = async () => {
+
+    // Reads from clipboard and splits by newline
+    const url_list = await navigator.clipboard.readText();
+    const url = url_list.split("\n");
+
+    // Opens new tab in loop
+    url.forEach(async (url) => {
+        try {
+            browser.tabs.create({ url: url });
+        }
+        catch(Error) {
+            output.innerHTML = Error;
+        }
+    })
+
 }
